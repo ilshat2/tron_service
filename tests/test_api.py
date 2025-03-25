2ce5c9aa17d2dae8fecb3bea50401fc6f75bd640
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-03-25 15:05:50 +0600

    test: Add integration tests for pagination

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 
 def test_wallet_info_endpoint(client, db):
     with patch('app.utils.get_tron_wallet_info') as mock_tron:
@@ -14,3 +16,13 @@ def test_wallet_info_endpoint(client, db):
 
         assert response.status_code == 200
         assert response.json()["balance"] == 100.0
+
+
+def test_get_requests_endpoint(client, db):
+    # Сначала создаем запись
+    client.post("/wallet/", json={"wallet_address": "TX111...111"})
+
+    # Проверяем пагинацию
+    response = client.get("/requests/?skip=0&limit=1")
+    assert len(response.json()) == 1
+    assert response.json()[0]["wallet_address"] == "TX111...111"
